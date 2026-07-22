d36c79b9aff6ca4ff0239a5d45dc0ad135d59f48
hazwanihanafi-commits <hazwanihanafi@gmail.com>
2026-07-22 16:30:53 +0800

    Update api.js

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,10 +1,7 @@
 import axios from "axios";
 
-const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
-  headers: {
-    "Content-Type": "application/json",
-  },
+export const api = axios.create({
+  baseURL:
+    import.meta.env.VITE_API_URL ||
+    "https://vivatrack-backend.onrender.com/api",
 });
-
-export default api;
