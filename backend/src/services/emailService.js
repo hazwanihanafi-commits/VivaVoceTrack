4bebec336cc52b2a892c82c88d41b5142dd4862d
hazwanihanafi-commits <hazwanihanafi@gmail.com>
2026-07-23 05:58:09 +0800

    Create emailService.js

---
+++ backend/src/services/emailService.js
@@ -0,0 +1,22 @@
+import nodemailer from "nodemailer";
+
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
+});
+
+export async function sendEmail({
+  to,
+  subject,
+  html,
+}) {
+  return transporter.sendMail({
+    from: process.env.EMAIL_FROM,
+    to,
+    subject,
+    html,
+  });
+}
