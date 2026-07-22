be32e62e60dd037156d5d18af31a8341fc7d7c90
hazwanihanafi-commits <hazwanihanafi@gmail.com>
2026-07-22 15:20:43 +0800

    Update MainLayout.jsx

--- frontend/src/layouts/MainLayout.jsx
+++ frontend/src/layouts/MainLayout.jsx
@@ -1,6 +1,7 @@
 import { Outlet } from "react-router-dom";
 import Sidebar from "../components/Sidebar";
 import Topbar from "../components/Topbar";
+import React from "react";
 
 export default function MainLayout() {
   return (
