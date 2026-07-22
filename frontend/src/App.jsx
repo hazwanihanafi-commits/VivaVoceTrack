fa918688721032f96912e7b6e38d02807c3324d8
hazwanihanafi-commits <hazwanihanafi@gmail.com>
2026-07-22 17:51:21 +0800

    Create App.jsx

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import { Routes, Route } from "react-router-dom";
+import MainLayout from "./layouts/MainLayout";
+import Dashboard from "./pages/Dashboard";
+import Students from "./pages/Students";
+
+export default function App() {
+  return (
+    <Routes>
+      <Route element={<MainLayout />}>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/students" element={<Students />} />
+      </Route>
+    </Routes>
+  );
+}
