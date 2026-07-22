0920bc4b65d843fd512f12d44ea62f9923c83719
hazwanihanafi-commits <hazwanihanafi@gmail.com>
2026-07-22 21:17:19 +0800

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import MainLayout from "./layouts/MainLayout";
 import Dashboard from "./pages/Dashboard";
 import Students from "./pages/Students";
+import VivaCases from "./pages/VivaCases";
 
 export default function App() {
   return (
@@ -9,6 +10,7 @@ export default function App() {
       <Route element={<MainLayout />}>
         <Route path="/" element={<Dashboard />} />
         <Route path="/students" element={<Students />} />
+        <Route path="/vivacases" element={<VivaCases />} />
       </Route>
     </Routes>
   );
