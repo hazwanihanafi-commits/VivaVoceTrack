a16866d7312d3908d4b3d29d53d395b46a6fd300
hazwanihanafi-commits <hazwanihanafi@gmail.com>
2026-07-22 12:40:47 +0800

    Create studentRoutes.js

---
+++ backend/src/routes/studentRoutes.js
@@ -0,0 +1,17 @@
+import express from "express";
+
+import {
+  getStudents,
+  getStudent,
+  createStudent,
+} from "../controllers/studentController.js";
+
+const router = express.Router();
+
+router.get("/", getStudents);
+
+router.get("/:id", getStudent);
+
+router.post("/", createStudent);
+
+export default router;
