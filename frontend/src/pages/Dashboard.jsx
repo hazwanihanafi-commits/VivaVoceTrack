1cdb9d3741c69d96d7663ab2273e61081876b072
hazwanihanafi-commits <hazwanihanafi@gmail.com>
2026-07-22 14:51:13 +0800

    Create Dashboard.jsx

---
+++ frontend/src/pages/Dashboard.jsx
@@ -0,0 +1,13 @@
+export default function Dashboard() {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold">
+        Dashboard
+      </h1>
+
+      <p className="mt-3">
+        Welcome to VivaTrack.
+      </p>
+    </div>
+  );
+}
