d2820d456255972d45b221ee6441f11d5a000d6d
hazwanihanafi-commits <hazwanihanafi@gmail.com>
2026-07-22 15:45:04 +0800

    Update Topbar.jsx

--- frontend/src/components/Topbar.jsx
+++ frontend/src/components/Topbar.jsx
@@ -1,9 +1,75 @@
 import React from "react";
+export default function Topbar(){
 
-export default function Topbar() {
-  return (
-    <header className="bg-white shadow p-4">
-      <h2 className="text-xl font-semibold">Dashboard</h2>
-    </header>
-  );
+return(
+
+<div
+
+className="
+
+h-20
+
+rounded-3xl
+
+bg-white/10
+
+backdrop-blur-2xl
+
+border
+
+border-white/20
+
+shadow-xl
+
+flex
+
+justify-between
+
+items-center
+
+px-8
+
+text-white
+
+">
+
+<div>
+
+<h2 className="text-3xl font-bold">
+
+Dashboard
+
+</h2>
+
+<p className="text-white/70">
+
+Universiti Sains Malaysia
+
+</p>
+
+</div>
+
+<div className="flex gap-5">
+
+<button className="text-2xl">
+
+🔔
+
+</button>
+
+<button className="text-2xl">
+
+⚙️
+
+</button>
+
+<div
+className="w-12 h-12 rounded-full bg-yellow-400"/>
+
+</div>
+
+</div>
+
+);
+
 }
