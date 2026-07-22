fa860f8515655d5764cfd17deecd4cabfd09c9b4
hazwanihanafi-commits <hazwanihanafi@gmail.com>
2026-07-22 16:22:57 +0800

    Update MainLayout.jsx

--- frontend/src/layouts/MainLayout.jsx
+++ frontend/src/layouts/MainLayout.jsx
@@ -1,19 +1,21 @@
-import { Outlet } from "react-router-dom";
 import Sidebar from "../components/Sidebar";
 import Topbar from "../components/Topbar";
+import { Outlet } from "react-router-dom";
 
 export default function MainLayout() {
   return (
-    <div className="min-h-screen">
+    <div className="bg-[#F4F6FA] min-h-screen">
 
       <Sidebar />
 
-      <main className="ml-72 p-8">
+      <main className="ml-64 p-8">
 
         <Topbar />
 
         <div className="mt-8">
+
           <Outlet />
+
         </div>
 
       </main>
