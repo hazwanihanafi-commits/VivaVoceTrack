9bd0d034b075dc44c36f800cc964b55f1f8f5c3d
hazwanihanafi-commits <hazwanihanafi@gmail.com>
2026-07-22 17:10:22 +0800

    Add files via upload

---
+++ frontend/src/layouts/MainLayout.jsx
@@ -0,0 +1,1 @@
+import {Outlet} from 'react-router-dom';import Sidebar from '../components/Sidebar';export default function(){return <div className='flex'><Sidebar/><main className='flex-1 p-6'><Outlet/></main></div>}
---
+++ frontend/src/routes/AppRoutes.jsx
@@ -0,0 +1,1 @@
+import {BrowserRouter,Routes,Route} from 'react-router-dom';import MainLayout from '../layouts/MainLayout';import Dashboard from '../pages/Dashboard';export default function(){return <BrowserRouter><Routes><Route element={<MainLayout/>}><Route path='/' element={<Dashboard/>}/></Route></Routes></BrowserRouter>}
