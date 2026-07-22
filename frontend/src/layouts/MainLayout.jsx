b7bd798e4e4c374fa13d1e3af0c64bd83d5dd8a7
hazwanihanafi-commits <hazwanihanafi@gmail.com>
2026-07-22 14:53:27 +0800

    Create MainLayout.jsx

---
+++ frontend/src/layouts/MainLayout.jsx
@@ -0,0 +1,19 @@
+import { Outlet } from "react-router-dom";
+import Sidebar from "../components/Sidebar";
+import Topbar from "../components/Topbar";
+
+export default function MainLayout() {
+  return (
+    <div className="flex min-h-screen bg-gray-100">
+      <Sidebar />
+
+      <div className="flex flex-1 flex-col">
+        <Topbar />
+
+        <main className="flex-1 p-6">
+          <Outlet />
+        </main>
+      </div>
+    </div>
+  );
+}
