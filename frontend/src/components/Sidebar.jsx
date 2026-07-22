3e6a2dc61db1d06cf52853bddf2819355253d6d2
hazwanihanafi-commits <hazwanihanafi@gmail.com>
2026-07-22 15:03:20 +0800

    Create Sidebar.jsx

---
+++ frontend/src/components/Sidebar.jsx
@@ -0,0 +1,7 @@
+export default function Sidebar() {
+  return (
+    <aside className="w-64 bg-indigo-900 text-white p-4">
+      <h1 className="text-xl font-bold">VivaTrack</h1>
+    </aside>
+  );
+}
