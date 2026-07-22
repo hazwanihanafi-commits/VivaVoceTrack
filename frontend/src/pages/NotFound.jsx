22b18e53f9d244fd0c1c65932b82b1040c1f95b3
hazwanihanafi-commits <hazwanihanafi@gmail.com>
2026-07-22 14:52:06 +0800

    Create NotFound.jsx

---
+++ frontend/src/pages/NotFound.jsx
@@ -0,0 +1,11 @@
+export default function NotFound() {
+  return (
+    <div className="p-10">
+      <h1 className="text-4xl font-bold">
+        404
+      </h1>
+
+      <p>Page not found.</p>
+    </div>
+  );
+}
