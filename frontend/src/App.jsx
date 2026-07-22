7f5aa0934251806d966b7239b9c898313224f0f6
hazwanihanafi-commits <hazwanihanafi@gmail.com>
2026-07-22 17:02:26 +0800

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,29 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
 
 import MainLayout from "./layouts/MainLayout";
-
 import Dashboard from "./pages/Dashboard";
-import Students from "./pages/Students";
 
 export default function App() {
   return (
     <BrowserRouter>
+
       <Routes>
+
         <Route element={<MainLayout />}>
-          <Route path="/" element={<Dashboard />} />
-          <Route path="/students" element={<Students />} />
+
+          <Route
+            path="/"
+            element={<Dashboard />}
+          />
+
         </Route>
+
       </Routes>
+
     </BrowserRouter>
   );
 }
