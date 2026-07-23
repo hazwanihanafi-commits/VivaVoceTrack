714a0ab234750167d376da5606ba43ad5e1c932d
hazwanihanafi-commits <hazwanihanafi@gmail.com>
2026-07-23 10:44:06 +0800

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import MainLayout from "./layouts/MainLayout";
 import Dashboard from "./pages/Dashboard";
 import Students from "./pages/Students";
 import VivaCases from "./pages/VivaCases";
+import Examiners from "./pages/Examiners";
 
 export default function App() {
   return (
@@ -11,6 +12,7 @@ export default function App() {
         <Route path="/" element={<Dashboard />} />
         <Route path="/students" element={<Students />} />
         <Route path="/vivacases" element={<VivaCases />} />
+          <Route path="/examiners" element={<Examiners />} />
       </Route>
     </Routes>
   );
