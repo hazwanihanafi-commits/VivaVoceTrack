ab06788750438c0bd8bcaba74fa0a6e529d492a4
hazwanihanafi-commits <hazwanihanafi@gmail.com>
2026-07-25 17:01:45 +0800

    Update vivaCaseRoutes.js

--- backend/src/routes/vivaCaseRoutes.js
+++ backend/src/routes/vivaCaseRoutes.js
@@ -1,21 +1,19 @@
 import express from "express";
 
 import {
-
-    getVivaCases,
-
-    getVivaCase,
-
-    createVivaCase
-
+  getVivaCases,
+  getVivaCase,
+  createVivaCase,
+  deleteVivaCase,
 } from "../controllers/vivaCaseController.js";
 
 const router = express.Router();
 
 router.get("/", getVivaCases);
-
 router.get("/:id", getVivaCase);
-
 router.post("/", createVivaCase);
+
+// NEW
+router.delete("/:id", deleteVivaCase);
 
 export default router;
