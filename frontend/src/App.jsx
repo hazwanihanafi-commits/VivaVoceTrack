3b7524cf47fb31214da65b5c84bbd159ceaa796c
hazwanihanafi-commits <hazwanihanafi@gmail.com>
2026-09-01 13:13:12 +0800

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,25 @@
 import { Routes, Route } from "react-router-dom";
 import MainLayout from "./layouts/MainLayout";
+
 import Dashboard from "./pages/Dashboard";
 import Students from "./pages/Students";
 import VivaCases from "./pages/VivaCases";
 import Examiners from "./pages/Examiners";
+import Schedule from "./pages/Schedule";
 
 export default function App() {
   return (
     <Routes>
       <Route element={<MainLayout />}>
         <Route path="/" element={<Dashboard />} />
+
         <Route path="/students" element={<Students />} />
+
         <Route path="/vivacases" element={<VivaCases />} />
-          <Route path="/examiners" element={<Examiners />} />
+
+        <Route path="/examiners" element={<Examiners />} />
+
+        <Route path="/schedule" element={<Schedule />} />
       </Route>
     </Routes>
   );
