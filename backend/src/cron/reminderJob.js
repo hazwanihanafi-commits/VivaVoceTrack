ab327e6880c261906f5b9f352cb52ace07200c84
hazwanihanafi-commits <hazwanihanafi@gmail.com>
2026-09-03 20:37:02 +0800

    Update reminderJob.js

--- backend/src/cron/reminderJob.js
+++ backend/src/cron/reminderJob.js
@@ -44,7 +44,7 @@ const EXAMINERS = [
 // RUN EVERY DAY AT 8:00 AM
 // ======================================================
 
-cron.schedule("* * * * *", async () => {
+cron.schedule("0 8 * * *", async () => {
 
   console.log(
     "Running Viva Report Reminder Job..."
