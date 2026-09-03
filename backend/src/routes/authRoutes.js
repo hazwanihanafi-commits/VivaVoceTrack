524e9689823de0c1e3f14c6b07d8781749de0ed3
hazwanihanafi-commits <hazwanihanafi@gmail.com>
2026-09-03 19:47:15 +0800

    Add Google authentication routes

---
+++ backend/src/routes/authRoutes.js
@@ -0,0 +1,20 @@
+import express from "express";
+
+import {
+  googleLogin,
+  googleCallback,
+} from "../controllers/authController.js";
+
+const router = express.Router();
+
+router.get(
+  "/google",
+  googleLogin
+);
+
+router.get(
+  "/google/callback",
+  googleCallback
+);
+
+export default router;
