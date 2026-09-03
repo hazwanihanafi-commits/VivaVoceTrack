9a442ce2384ea6929550106dc6e6569619c32838
hazwanihanafi-commits <hazwanihanafi@gmail.com>
2026-09-03 08:17:03 +0800

    Update reminderEmail.js

--- backend/src/templates/reminderEmail.js
+++ backend/src/templates/reminderEmail.js
@@ -1,22 +1,52 @@
 import emailLayout from "./emailLayout.js";
 
-export default function reminderEmail() {
+export default function reminderEmail(data = {}) {
+
   return emailLayout(
     "Reminder: Examiner Report",
+
     `
-<p>Dear {{ExaminerTitle}} {{ExaminerName}},</p>
+<p>
+Dear ${data.ExaminerTitle || ""} ${data.ExaminerName || "Examiner"},
+</p>
 
 <p>
 This is a friendly reminder that the examiner report for the following candidate is due on
-<strong>{{ReportDueDate}}</strong>.
+<strong>${data.ReportDueDate || "-"}</strong>.
 </p>
 
-<p><strong>Student:</strong> {{StudentName}}</p>
-<p><strong>Thesis:</strong> {{ThesisTitle}}</p>
+<p>
+<strong>Student:</strong>
+${data.StudentName || "-"}
+</p>
 
-<p>Thank you for your cooperation.</p>
+<p>
+<strong>Matric No.:</strong>
+${data.MatricNo || "-"}
+</p>
 
-<p>VivaTrack Secretariat</p>
+<p>
+<strong>Programme:</strong>
+${data.Programme || "-"}
+</p>
+
+<p>
+<strong>Thesis:</strong>
+${data.ThesisTitle || "-"}
+</p>
+
+<p>
+Please submit your examiner report before the stated deadline.
+</p>
+
+<p>
+Thank you for your cooperation.
+</p>
+
+<p>
+<strong>VivaTrack Secretariat</strong><br>
+Universiti Sains Malaysia
+</p>
 `
   );
 }
